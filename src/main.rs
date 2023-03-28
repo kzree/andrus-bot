9109b82c75180bb63743aafe123e51a4abfaf780
“Erkki <43129445+kzree@users.noreply.github.com>
2023-03-28 17:38:33 +0000

    Add play command

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,60 @@
-use dotenv::dotenv;
 use std::env;
+
+use dotenv::dotenv;
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, CommandResult};
+
+#[group]
+#[commands(ping)]
+#[commands(play)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    let token = env::var("DISCORD_TOKEN").expect("token");
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("-"))
+        .group(&GENERAL_GROUP);
 
-    println!("{}", token);
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
 }
 
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn play(ctx: &Context, msg: &Message) -> CommandResult {
+    let msg_pieces: Vec<&str> = msg.content.split_whitespace().collect();
+
+    if msg_pieces.len() > 1 {
+        msg.reply(ctx, format!("Your link: {}", msg_pieces[1])).await?;
+    } else {
+        msg.reply(ctx, "Missing link").await?;
+    }
+
+    Ok(())
+}
